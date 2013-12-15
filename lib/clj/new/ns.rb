ff4115f4a5c844e8eb800827762ef349bb2f8dd3
Waldemar <waldemar.schwan@adcloud.com>
2013-12-15 23:23:08 +0100

    init

---
+++ lib/clj/new/ns.rb
@@ -0,0 +1,9 @@
+require "clj/new/ns/version"
+
+module Clj
+  module New
+    module Ns
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/clj/new/ns/version.rb
@@ -0,0 +1,7 @@
+module Clj
+  module New
+    module Ns
+      VERSION = "0.0.1"
+    end
+  end
+end
